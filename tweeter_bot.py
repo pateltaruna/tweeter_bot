8f09768e37a4720c6942688d4574718b4e0a2596
taruna <tarunapatel1998@gmail.com>
2020-08-20 11:42:46 +0530

    committed

--- tweeter_bot.py
+++ tweeter_bot.py
@@ -7,13 +7,13 @@ auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
 api = tweepy.API(auth)
 
 # mention = api.mentions_timeline()
-#to find id of recent tweet
+##to find id of recent tweet
 # print(mention[0].text)
 # print(mention[0].id)
 
 # for tweets in mention:
-#     print(tweets.text)
-#     if "hiii maddie" in tweets.text.lower():
+#     print(tweets.text + '  ' + str(tweets.id))
+#     if "hiii " in tweets.text.lower():
 #         print("found")
 
 #lastseen_id.txt contains the id of the last responded tweet so that we start responding from the next tweet
@@ -33,17 +33,19 @@ def store_lastseen_id(file,id):
     return
 
 def reply_to_tweets():
-    last_seen_id = retrieve_lastseen_id(lastseen_id.txt)
-    #the below line provides all the tweets with id greater than lastseen id
+    # mentions = api.mentions_timeline()
+    last_seen_id = retrieve_lastseen_id("last_seen_id.txt")
+    # the below line provides all the tweets with id greater than lastseen id
     mentions = api.mentions_timeline( last_seen_id,tweet_mode='extended')  # NOTE: We need to use tweet_mode='extended' below to show all full tweets (with full_text). Without it, long tweets would be cut off.
     for tweets in reversed(mentions):
-        last_seen_id = mentions.id
-        store_lastseen_id(lastseen_id.txt,last_seen_id)
-        if "hiii maddie" in tweets.text.lower():
-            print(mention.id + '_' + mention.txt + "found")
-            api.update_status('@' + mentions.user.screen_name +'#Its a reply back to you!', mention.id)
+        last_seen_id = tweets.id
+        print(str(tweets.id) + '_' + tweets.full_text )
+        store_lastseen_id("last_seen_id.txt",last_seen_id)
+        if "hiii " in tweets.full_text.lower():
+            print('found & replying to '+'  '+str(tweets.id) + '_' + tweets.full_text )
+            api.update_status('@' + tweets.user.screen_name +'#Its a reply back to you!', tweets.id)
 
 
 while True:
     reply_to_tweets()
-    time.sleep(5)
+    time.sleep(1)
